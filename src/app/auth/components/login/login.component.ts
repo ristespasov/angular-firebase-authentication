4d7790095730a678e21e4c5abf089dfb4a022a74
ristespasov <ristespasov@hotmail.com>
2021-12-30 18:06:50 +0100

    login component

---
+++ src/app/auth/components/login/login.component.ts
@@ -0,0 +1,60 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Subject, take, takeUntil } from 'rxjs';
+import { ILoginPayload } from '../../interfaces/login.interface';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['../../styles/auth.component.scss'],
+})
+export class LoginComponent {
+  destroy$ = new Subject();
+  hide = true;
+
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+  });
+
+  constructor(private authService: AuthService) {}
+
+  get email(): FormControl {
+    return this.loginForm.get('email') as FormControl;
+  }
+
+  get password(): FormControl {
+    return this.loginForm.get('password') as FormControl;
+  }
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    const formValue = this.loginForm.value;
+
+    const loginPayload: ILoginPayload = {
+      email: formValue.email,
+      password: formValue.password,
+      returnSecureToken: true,
+    };
+
+    this.authService
+      .login(loginPayload)
+      .pipe(take(1), takeUntil(this.destroy$))
+      .subscribe({
+        next: (response) => {
+          console.log('SUCCESS:', response);
+        },
+        error: (err) => {
+          console.error('ERROR:', err.error.error.message);
+        },
+        complete: () => {
+          console.info('COMPLETE:', 'Completed!');
+        },
+      });
+  }
+}
