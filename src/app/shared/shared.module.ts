06c34f48a6ddc99d512980b122d008579ec65df7
ristespasov <ristespasov@hotmail.com>
2021-12-30 18:12:37 +0100

    new material modules added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,18 @@
 import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { AuthRoutingModule } from '../auth/auth-routing.module';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LocaleComponent } from './components/locale/locale.component';
 
 @NgModule({
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   declarations: [
     // components
     HeaderComponent,
@@ -16,10 +21,15 @@ import { LocaleComponent } from './components/locale/locale.component';
   ],
   imports: [
     CommonModule,
+    AuthRoutingModule,
+
     // material
     MatIconModule,
     MatToolbarModule,
     MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
   ],
   exports: [
     // components
@@ -31,6 +41,9 @@ import { LocaleComponent } from './components/locale/locale.component';
     MatIconModule,
     MatToolbarModule,
     MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
   ],
 })
 export class SharedModule {}
