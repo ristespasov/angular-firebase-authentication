ae6f8165dd0978e3d5399d047fb0116e4b1e7995
ristespasov <ristespasov@hotmail.com>
2021-12-30 23:29:57 +0100

    alert service

---
+++ src/app/shared/services/alert.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { AlertComponent } from '../components/alert/alert.component';
+import { AlertType } from '../enums/alert.enum';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AlertService {
+  constructor(private snackBar: MatSnackBar) {}
+  public openSnackBar(message: string, snackType?: AlertType) {
+    const alertType: AlertType =
+      snackType !== undefined ? snackType : AlertType.Error;
+
+    this.snackBar.openFromComponent(AlertComponent, {
+      duration: 6000,
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+      data: { message: message, snackType: alertType },
+    });
+  }
+}
