f769d8f45daa3b17e3248cd4cdd10a4b65795d1f
ristespasov <ristespasov@hotmail.com>
2021-12-31 18:26:52 +0100

    logout functionality

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  userSub = new Subscription();
+  isAuthenticated = false;
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {
+    this.userSub = this.authService.user.subscribe((user) => {
+      this.isAuthenticated = !!user;
+    });
   }
 
+  onLogout() {
+    this.authService.logout();
+    this.router.navigate(['login']);
+  }
+
+  ngOnDestroy() {
+    this.userSub.unsubscribe();
+  }
 }
