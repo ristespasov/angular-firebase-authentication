217b247ea822bfd24869250e673634cab0f74608
ristespasov <ristespasov@hotmail.com>
2021-12-31 19:01:15 +0100

    new method defined

--- src/app/shared/services/alert.service.ts
+++ src/app/shared/services/alert.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { AlertComponent } from '../components/alert/alert.component';
 import { AlertType } from '../enums/alert.enum';
+import { AuthErrorMessageType } from '../enums/auth-message.enum';
 
 @Injectable({
   providedIn: 'root',
@@ -18,5 +19,27 @@ export class AlertService {
       verticalPosition: 'top',
       data: { message: message, snackType: alertType },
     });
+  }
+
+  handleAuthErrorMessage(value: any) {
+    switch (value) {
+      case AuthErrorMessageType.EmailExists:
+        return (value =
+          'The email address is already in use by another account.');
+      case AuthErrorMessageType.OperationNotAlowed:
+        return (value = 'Password sign-in is disabled for this project.');
+      case AuthErrorMessageType.TooManyAttemptsTryLater:
+        return (value =
+          'We have blocked all requests from this device due to unusual activity. Try again later.');
+      case AuthErrorMessageType.EmailNotFound:
+        return (value =
+          'There is no user record corresponding to this identifier. The user may have been deleted.');
+      case AuthErrorMessageType.InvalidPassword:
+        return (value = 'The password you entered is incorrect.');
+      case AuthErrorMessageType.UserDisabled:
+        return (value =
+          'The user account has been disabled by an administrator.');
+    }
+    return value;
   }
 }
