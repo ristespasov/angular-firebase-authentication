091bf9364d90f652b16e397e4e8f6f91c249b19d
ristespasov <ristespasov@hotmail.com>
2022-01-02 01:15:22 +0100

    new messages added

---
+++ src/app/auth/enums/auth-message.enum.ts
@@ -0,0 +1,15 @@
+export enum AuthErrorMessageType {
+  EmailExists = 'EMAIL_EXISTS',
+  OperationNotAlowed = 'OPERATION_NOT_ALLOWED',
+  TooManyAttemptsTryLater = 'TOO_MANY_ATTEMPTS_TRY_LATER',
+  EmailNotFound = 'EMAIL_NOT_FOUND',
+  InvalidPassword = 'INVALID_PASSWORD',
+  UserDisabled = 'USER_DISABLED',
+}
+
+export enum AuthSuccessMessageType {
+  RegisterSuccess = 'Successfully registered!',
+  LoginSuccess = 'Successfully logged in!',
+  ResetEmailSentSuccess = 'An email with a reset link was successfully sent to you!',
+  ResetPasswordSuccess = 'Your password was successfully reset!',
+}
