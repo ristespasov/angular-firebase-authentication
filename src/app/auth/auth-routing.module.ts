6538806ced959b79fa3d78383b4229bc183317b3
ristespasov <ristespasov@hotmail.com>
2022-01-02 23:58:39 +0100

    home guard applied

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -4,24 +4,29 @@ import { ForgotPasswordComponent } from './components/forgot-password/forgot-pas
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import { HomeGuard } from './guards/home.guard';
 import { RedirectGuard } from './guards/redirect.guard';
 
 const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
+    canActivate: [HomeGuard],
   },
   {
     path: 'register',
     component: RegisterComponent,
+    canActivate: [HomeGuard],
   },
   {
     path: 'forgot-password',
     component: ForgotPasswordComponent,
+    canActivate: [HomeGuard],
   },
   {
     path: 'reset-password',
     component: ResetPasswordComponent,
+    canActivate: [HomeGuard],
   },
   {
     path: '__/auth/action',
