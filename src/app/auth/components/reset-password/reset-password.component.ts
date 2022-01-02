b8b306cbb731ca9b32d6efaa57874cae8536d933
ristespasov <ristespasov@hotmail.com>
2022-01-02 01:17:25 +0100

    reset password component

---
+++ src/app/auth/components/reset-password/reset-password.component.ts
@@ -0,0 +1,85 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Subject, take, takeUntil } from 'rxjs';
+import { validationPatterns } from 'src/app/shared/constants/validation-patterns.constants';
+import { AlertType } from 'src/app/shared/enums/alert.enum';
+import { AlertService } from 'src/app/shared/services/alert.service';
+import { AuthSuccessMessageType } from '../../enums/auth-message.enum';
+import { IResetPasswordPayload } from '../../interfaces/reset-password.interface';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-reset-password',
+  templateUrl: './reset-password.component.html',
+  styleUrls: ['../../styles/auth.component.scss'],
+})
+export class ResetPasswordComponent implements OnInit {
+  destroy$ = new Subject();
+  isHidden: boolean = true;
+  isSpinning: boolean = false;
+
+  resetPasswordForm = new FormGroup({
+    password: new FormControl('', [
+      Validators.required,
+      Validators.pattern(validationPatterns.password),
+    ]),
+  });
+
+  constructor(
+    private authService: AuthService,
+    private alertService: AlertService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {}
+
+  get password(): FormControl {
+    return this.resetPasswordForm.get('password') as FormControl;
+  }
+
+  onSubmit() {
+    if (this.resetPasswordForm.invalid) {
+      this.resetPasswordForm.markAllAsTouched();
+      return;
+    }
+
+    this.isSpinning = true;
+
+    const formValue = this.resetPasswordForm.value;
+
+    const resetPasswordPayload: IResetPasswordPayload = {
+      oobCode: '',
+      password: formValue.password,
+    };
+
+    this.authService
+      .resetPassword(resetPasswordPayload)
+      .pipe(take(1), takeUntil(this.destroy$))
+      .subscribe({
+        next: (response) => {
+          this.isSpinning = false;
+          this.alertService.openSnackBar(
+            AuthSuccessMessageType.ResetPasswordSuccess,
+            AlertType.Success
+          );
+          this.router.navigate(['login']);
+        },
+        error: (err) => {
+          this.isSpinning = false;
+          this.alertService.openSnackBar(
+            err.error.error.message,
+            AlertType.Error
+          );
+        },
+        complete: () => {
+          console.info('COMPLETE:', 'Completed!');
+        },
+      });
+  }
+
+  onDestroy(): void {
+    this.destroy$.next(void 0);
+    this.destroy$.complete();
+  }
+}
