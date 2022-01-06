ab131ba7f47f395b2f65127c8a1ca155abfc361e
ristespasov <ristespasov@hotmail.com>
2022-01-06 16:06:15 +0100

    env prod updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,9 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://identitytoolkit.googleapis.com/v1/',
-  registerUrl: 'accounts:signUp?key=',
-  loginUrl: 'accounts:signInWithPassword?key=',
+  authBaseUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:',
+  registerUrl: 'signUp?key=',
+  loginUrl: 'signInWithPassword?key=',
+  forgotPasswordUrl: 'sendOobCode?key=',
+  resetPasswordUrl: 'resetPassword?key=',
   API_KEY: 'AIzaSyDR121oGzffqrz-S-KyTMihEObF1a2f1E8',
 };
